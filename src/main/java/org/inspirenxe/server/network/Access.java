d5ea7e9d2db30a30226ad9baef08a49faafd589c
Steven Downer <grinch@outlook.com>
2014-02-08 01:32:53 -0600

    Add isBanned and isWhitelisted methods
    Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/org/inspirenxe/server/network/Access.java
+++ src/main/java/org/inspirenxe/server/network/Access.java
@@ -68,16 +68,16 @@ public final class Access {
     }
 
     /**
-     * Checks if the banlist is enabled
+     * Checks if the banlist is enabled.
      *
-     * @return true if the banlist is enabled, false if not
+     * @return true if the banlist is enabled, false if not.
      */
     public synchronized boolean isBanlistEnabled() {
         return CONFIGURATION.getChild(BANLIST_KEY).getChild(ENABLED_KEY).getBoolean();
     }
 
     /**
-     * Checks if the whitelist is enabled
+     * Checks if the whitelist is enabled.
      *
      * @return true if the whitelist is enabled, false if not.
      */
@@ -86,69 +86,99 @@ public final class Access {
     }
 
     /**
-     * Sets the enabled status of the banlist
+     * Sets the enabled status of the banlist.
      *
-     * @param enabled true to enable the banlist, false to disable it
+     * @param enabled True to enable the banlist, false to disable it.
      */
     public synchronized void setBanlistEnabled(boolean enabled) {
         CONFIGURATION.getChild(BANLIST_KEY).getChild(ENABLED_KEY).setValue(boolean.class, enabled);
     }
 
     /**
-     * Sets the enabled status of the whitelist
+     * Sets the enabled status of the whitelist.
      *
-     * @param enabled true to enable the whitelist, false to disable it.
+     * @param enabled True to enable the whitelist, false to disable it.
      */
     public synchronized void setWhitelistEnabled(boolean enabled) {
         CONFIGURATION.getChild(WHITELIST_KEY).getChild(ENABLED_KEY).setValue(boolean.class, enabled);
     }
 
     /**
-     * Gets the current banlist
+     * Gets the current banlist.
      *
-     * @return the list of players in the banlist
+     * @return The list of players in the banlist.
      */
     public synchronized List<String> getBanlist() {
         return Collections.unmodifiableList(BANLIST);
     }
 
     /**
-     * Gets the current whitelist
+     * Gets the current whitelist.
      *
-     * @return the list of players in the whitelist
+     * @return The list of players in the whitelist.
      */
     public synchronized List<String> getWhitelist() {
         return Collections.unmodifiableList(CONFIGURATION.getChild(WHITELIST_KEY).getChild(LIST_KEY).getStringList());
     }
 
     /**
-     * Sets the current banlist list
+     * Sets the current banlist list.
      *
-     * @param list the list to save to the configuration
+     * @param list The list to save to the configuration.
      */
     public synchronized void setBanlist(List<String> list) {
         CONFIGURATION.getChild(BANLIST_KEY).getChild(LIST_KEY).setValue(List.class, list);
     }
 
     /**
-     * Sets the current whitelist list
+     * Sets the current whitelist list.
      *
-     * @param list the list to save to the configuration
+     * @param list The list to save to the configuration.
      */
     public synchronized void setWhitelist(List<String> list) {
         CONFIGURATION.getChild(WHITELIST_KEY).getChild(LIST_KEY).setValue(List.class, list);
     }
 
     /**
-     * Adds or removes a player to/from the banlist
+     * Checks if a name is in the banlist.
      *
-     * @param name the player name
-     * @param add if true it will add the player to the banlist, otherwise it will remove them
-     * @return if the player was successfully added or removed
+     * @param name The name to check.
+     * @return True if banned, false if not.
+     */
+    public synchronized boolean isBanned(String name) {
+        for (String entry : BANLIST) {
+            if (entry.equalsIgnoreCase(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if a name is in the whitelist.
+     *
+     * @param name The name to check.
+     * @return True if whitelisted, false if not.
+     */
+    public synchronized boolean isWhitelisted(String name) {
+        for (String entry : WHITELIST) {
+            if (entry.equalsIgnoreCase(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Adds or removes a player to/from the banlist.
+     *
+     * @param name The player name.
+     * @param add If true it will add the player to the banlist, otherwise it will remove them.
+     * @return If the player was successfully added or removed.
      */
     public synchronized boolean ban(String name, boolean add) {
         if (add) {
-            if (!BANLIST.contains(name) && BANLIST.add(name)) {
+            if (!isBanned(name) && BANLIST.add(name)) {
                 setBanlist(BANLIST);
                 return true;
             }
@@ -162,15 +192,15 @@ public final class Access {
     }
 
     /**
-     * Adds or removes a player to/from the whitelist
+     * Adds or removes a player to/from the whitelist.
      *
-     * @param name the player name
-     * @param add if true it will add the player to the whitelist, otherwise it will remove them
-     * @return if the player was successfully added or removed
+     * @param name The player name.
+     * @param add If true it will add the player to the whitelist, otherwise it will remove them.
+     * @return If the player was successfully added or removed.
      */
     public synchronized boolean whitelist(String name, boolean add) {
         if (add) {
-            if (!WHITELIST.contains(name) && WHITELIST.add(name)) {
+            if (!isWhitelisted(name) && WHITELIST.add(name)) {
                 setWhitelist(WHITELIST);
                 return true;
             }
