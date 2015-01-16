0a7cc07eec98c16052f80db7ddb71ea5b02dc577
Steven Downer <grinch@outlook.com>
2015-01-16 17:10:11 -0600

    Update license headers
    Signed-off-by: Steven Downer <grinch@outlook.com>

--- src/main/java/org/inspirenxe/server/network/codec/play/KeepAliveCodec.java
+++ src/main/java/org/inspirenxe/server/network/codec/play/KeepAliveCodec.java
@@ -1,7 +1,7 @@
 /**
  * This file is part of Pulse, licensed under the MIT License (MIT).
  *
- * Copyright (c) 2014 InspireNXE <http://inspirenxe.org/>
+ * Copyright (c) 2014-2015 InspireNXE <http://inspirenxe.org/>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
